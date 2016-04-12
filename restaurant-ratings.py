c395a899346c0c38bb3cf8e56393cafa0a9fbcd0
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 17:01:55 -0700

    completed exercise: prints a list of restaurants and ratings

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,1 +1,39 @@
-# your code goes here
+"""
+
+"""
+scores={}
+restaurants_output={}
+sorted_scores=[]
+
+file_scores = open("scores.txt")
+
+for scores in file_scores:
+	#push all the elements into our list 
+	#remove the \n
+	scores = scores.rstrip()
+	sorted_scores.append(scores)
+
+sorted_scores=sorted(sorted_scores)
+print(sorted_scores)
+
+for entry in sorted_scores:
+	info_pair = entry.split(",")
+	#print(info_pair)
+
+
+	for restaurant in info_pair:
+		#len(restaurant)
+		#we need to search the string and find ':'
+		#and then split the string at the ':' creating two variables
+		if ":" in restaurant:
+			index_number = restaurant.index(":")
+			
+			restaurant_name = restaurant[:index_number]
+			restaurant_rating = restaurant[index_number+1:]
+
+			
+			print restaurant_name, "is rated at", restaurant_rating
+			#print restaurant_rating
+			#restaurants_output[restaurant_name] = restaurant_rating
+
+#print(restaurants_output)
